7eb2568ff1a37df82bb7a8111b08dde764a4bb8f
sonusah5268 <58986067+sonusah5268@users.noreply.github.com>
2021-08-27 11:44:31 +0530

    Add files via upload

---
+++ src/components/SliderData.js
@@ -0,0 +1,22 @@
+export const SliderData = [
+  {
+    image:
+    'https://static.wixstatic.com/media/6ab06b_5d486de9f315470694c6c3ebab2c68d4~mv2.jpg/v1/fill/w_560,h_438,al_c,q_80,usm_0.66_1.00_0.01/tech_JPG.webp'
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1501446529957-6226bd447c46?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1489&q=80'
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1483729558449-99ef09a8c325?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80'
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1475189778702-5ec9941484ae?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1351&q=80'
+  },
+  {
+    image:
+      'http://isha.sadhguru.org/blog/wp-content/uploads/2016/05/natures-temples.jpg'
+  }
+];
